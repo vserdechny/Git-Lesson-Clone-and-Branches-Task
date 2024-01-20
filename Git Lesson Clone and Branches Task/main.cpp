ac01b34bf410a580c86091f8abc70619b4341afc
Виталий Сердечный <serdechny_family_ua@outlook.com>
2024-01-20 14:29:33 +0200

    My first coomit in new branch

--- Git Lesson Clone and Branches Task/main.cpp
+++ Git Lesson Clone and Branches Task/main.cpp
@@ -1,10 +1,29 @@
 #include "Person.h"
 #include <iostream>
+#include <string>
 
 using std::cout;
 using std::cin;
+using std::endl;
 
 int main()
 {
-	// TODO -> Insert your test code here
+	Person testPerson;
+
+	cout << "Enter first name: ";
+	getline(cin, testPerson.firstName);
+	cout << "Enter last name: ";
+	getline(cin, testPerson.lastName);
+	cout << "Enter age: ";
+	cin >> testPerson.age;
+	cout << "Enter height (m): ";
+	cin >> testPerson.height;
+	cout << "Enter weight (kg): ";
+	cin >> testPerson.weight;
+
+	cout << "\n\nYour test person:" << endl;
+	cout << "Full name: " << testPerson.firstName << " " << testPerson.lastName << endl;
+	cout << "Age: " << testPerson.age << " y.o." << endl;
+	cout << "Height: " << testPerson.height << " m" << endl;
+	cout << "Weight: " << testPerson.weight << " kg" << endl;
 }
