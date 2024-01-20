158dc19b27ad334d8a2d2e74f2ca126f3ff85c67
Виталий Сердечный <serdechny_family_ua@outlook.com>
2024-01-20 11:36:00 +0200

    Добавьте файлы проекта.

---
+++ Git Lesson Clone and Branches Task/Person.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+
+using std::string;
+
+struct Person
+{
+	string firstName, lastName;
+	int age;
+	double height, weight;
+};
---
+++ Git Lesson Clone and Branches Task/main.cpp
@@ -0,0 +1,10 @@
+#include "Person.h"
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main()
+{
+	// TODO -> Insert your test code here
+}
